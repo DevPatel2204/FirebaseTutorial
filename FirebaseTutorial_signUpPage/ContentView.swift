e33192e60e62d24147f497cae861d3c541293e9e
Dev Patel <dpatel222430@gmail.com>
2024-06-17 16:23:37 +0530

    Initial Commit

---
+++ FirebaseTutorial_signUpPage/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FirebaseTutorial_signUpPage
+//
+//  Created by Dev Patel on 17/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FirebaseTutorial_signUpPage/FirebaseTutorial_signUpPageApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseTutorial_signUpPageApp.swift
+//  FirebaseTutorial_signUpPage
+//
+//  Created by Dev Patel on 17/06/24.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseTutorial_signUpPageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
